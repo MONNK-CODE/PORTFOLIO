7c458d47036a795f0249dc1d7a2f3449d3853036
Muhais Olatundun <monnkcode@gmail.com>
2024-04-17 19:47:46 +0000

    Updated Js security

--- contact.js
+++ contact.js
@@ -1,42 +1,78 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var form = document.getElementById('contactForm');
+//   if (form) {
+//     form.addEventListener('submit', function(event) {
+//       event.preventDefault();
+
+//       //This Extracts the data from the user in the form 
+//       var firstName = document.querySelector('[name="first_name"]').value;
+//       var lastName = document.querySelector('[name="last_name"]').value;
+//       var emailAddress = document.querySelector('[name="email"]').value;
+//       var message = document.querySelector('[name="message"]').value;
+
+//       // this Sends email using EmailJS API
+//       emailjs.send("", "", { // Service ID and Template ID
+//           "firstName": firstName,
+//           "lastName": lastName,
+//           "emailAddress": emailAddress,
+//           "message": message,
+//       })
+//       .then(function(response) {
+//          console.log('SUCCESS!', response.status, response.text);
+//          // USING SweetAlert for success message
+//          swal({
+//             title: "Success!",
+//             text: "Your message has been sent successfully!",
+//             icon: "success",
+//           });
+//          form.reset(); //this resets the input fields
+//       }, function(error) {
+//          console.log('FAILED...', error);
+//          // Use SweetAlert for error message
+//          swal({
+//             title: "Failed!",
+//             text: "Failed to send the message, please try again.",
+//             icon: "error",
+//           });
+//       });
+//     });
+//   } else {
+//     console.log('Form not found');
+//   }
+// });
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var form = document.getElementById('contactForm');
   if (form) {
     form.addEventListener('submit', function(event) {
       event.preventDefault();
 
-      //This Extracts the data from the user in the form 
-      var firstName = document.querySelector('[name="first_name"]').value;
-      var lastName = document.querySelector('[name="last_name"]').value;
-      var emailAddress = document.querySelector('[name="email"]').value;
-      var message = document.querySelector('[name="message"]').value;
+      // Extract the data from the form
+      var Name = document.querySelector('[name="first_name"]').value;
+      var emailAddress = document.querySelector('[name="email_address"]').value;
 
-      // this Sends email using EmailJS API
-      emailjs.send("", "", { // Service ID and Template ID
-          "firstName": firstName,
-          "lastName": lastName,
-          "emailAddress": emailAddress,
-          "message": message,
-      })
+      console.log("Name:", Name);
+      console.log("Email Address:", emailAddress);
+
+      // Send email using EmailJS
+      emailjs.send(process.env.EMAILJS_SERVICE_ID, process.env.EMAILJS_TEMPLATE_ID, { 
+          "emailAddress": emailAddress,  
+          "Name": Name,      
+       }, process.env.EMAILJS_PUBLIC_KEY)
       .then(function(response) {
          console.log('SUCCESS!', response.status, response.text);
-         // Use SweetAlert for success message
-         swal({
-            title: "Success!",
-            text: "Your message has been sent successfully!",
-            icon: "success",
-          });
-         form.reset(); //this resets the input fields
+         alert('Your message has been sent successfully!');
+         // This Reset the form
+         form.reset();
       }, function(error) {
          console.log('FAILED...', error);
-         // Use SweetAlert for error message
-         swal({
-            title: "Failed!",
-            text: "Failed to send the message, please try again.",
-            icon: "error",
-          });
+         alert('Failed to send the message, please try again.');
       });
     });
   } else {
     console.log('Form not found');
   }
 });
+
--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   let ld = document.querySelector(".ld");
-//   let discord = document.querySelector(".discord");
-//   let email = document.querySelector(".email");
-//   let git = document.querySelector(".git");
-//   let contact = document.querySelector(".contact");
-//   let icons = document.querySelector(".icons");
-
-//   contact.addEventListener("click", function() {
-//     console.log("onclick");
-//     // ld.style.display = "block";
-//     // discord.style.display = "block";
-//     // email.style.display = "block";
-//     // git.style.display = "block";
-//     icons.style.display = "block";
-//   });
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   var contactButton = document.querySelector(".contact");
-//   var icons = document.querySelectorAll(".icons a i");
-
-//   contactButton.addEventListener("click", function() {
-//     // Toggle the visibility of each icon
-//     icons.forEach(function(icon) {
-//       icon.classList.toggle("show");
-//     });
-//   });
-// });
-
-
-
 function downloadResume() {
   var link = document.createElement('a');
   link.href = 'RESUME.pdf';
