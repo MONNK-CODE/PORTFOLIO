7937c3a7f46241383f0e060726bbfce3aa068e24
Muhais Olatundun <monnkcode@gmail.com>
2024-07-21 03:51:18 +0000

    Website Device Optimization

--- project.js
+++ project.js
@@ -52,15 +52,13 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-
 document.addEventListener("DOMContentLoaded", function() {
   const filterButtons = document.querySelectorAll(".filter-btn");
 
   filterButtons.forEach(button => {
     button.addEventListener("click", function() {
-      // Remove active class from all buttons
+    
       filterButtons.forEach(btn => btn.classList.remove("active"));
-      // Add active class to the clicked button
       this.classList.add("active");
     });
   });
