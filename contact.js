7f128c432e3abbc916878b16a9be4ecee52867f6
Muhais Olatundun <monnkcode@gmail.com>
2024-09-19 17:43:08 +0000

    security measures

--- contact.js
+++ contact.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   (function(){
-     emailjs.init("ggYLuSeAb8KgHjiOv"); // THIS IS MY PUBLIC KEY 
+     emailjs.init(process.env.EMAILJS_PUBLIC_KEY);  // Access the key from .env; // THIS IS MY PUBLIC KEY 
   })();
 
   var form = document.getElementById('contactForm');
