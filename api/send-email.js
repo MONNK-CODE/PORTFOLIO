19755976445cfbe8da1ef62ce10369c5052e8f2c
mon <monnkcode@gmail.com>
2025-05-21 13:57:39 -0500

    Log EMAILJS_PUBLIC_KEY for debugging purposes

--- api/send-email.js
+++ api/send-email.js
@@ -42,3 +42,4 @@ export default async function handler(req, res) {
         return res.status(500).json({ success: false, error: err.message });
     }
 }
+console.log("EMAILJS_PUBLIC_KEY =", process.env.EMAILJS_PUBLIC_KEY);
