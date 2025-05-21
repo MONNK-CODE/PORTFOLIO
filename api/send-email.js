8b2c857b8d56fc87198b246d674fe5a9502b6632
mon <monnkcode@gmail.com>
2025-05-21 14:39:37 -0500

    Enable logging for email sending debug purposes
    Re-added a console log statement to facilitate debugging email send requests. This helps track data being sent to EmailJS for troubleshooting issues effectively.

--- api/send-email.js
+++ api/send-email.js
@@ -37,9 +37,9 @@ export default async function handler(req, res) {
         return res.status(500).json({ success: false, error: err.message });
     }
 }
-// console.log("Sending to EmailJS:", {
-//     first_name,
-//     last_name,
-//     email,
-//     message,
-// });
+console.log("Sending to EmailJS:", {
+    first_name,
+    last_name,
+    email,
+    message,
+});
