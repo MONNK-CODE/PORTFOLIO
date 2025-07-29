5a01783d721148120f9516da0976aca1362e98d0
mon <monnkcode@gmail.com>
2025-07-28 21:27:31 -0500

    transfer of project bounty friebase storage

--- api/notify.js
+++ api/notify.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
     try {
         const response = await fetch(
-            "https://script.google.com/macros/s/AKfycbwkXarbHSUfs_APaz3_GOd8hVw2xDk_CCxbiWQix2Lixs1-6YweVybY89-GVvB9mAHW/exec",
+            "https://script.google.com/macros/library/d/1FVSv42vszAmK4obu4haIbN0bdiP0XrxSPrsbAAOb26AgAxsVRFXnBdLt/2",
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
